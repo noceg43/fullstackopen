c47010f81003881936f91bee20a7603e0bdfba08
noceg43 <noceg43@gmail.com>
2025-01-25 12:29:08 +0100

    add: part 3c

---
+++ part3/mongodb/index.js
@@ -0,0 +1,131 @@
+// do not use ES module it's not suggested in this course
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
+
+
+const app = express()
+app.use(cors())
+
+
+// on the dist folder, we have the frontend
+// when the server receives a GET request this middleware is used
+app.use(express.static('dist'))
+
+
+// activate json parser
+// it's a middleware that parses incoming requests with JSON payloads
+app.use(express.json())
+
+
+// another middleware used to log
+const requestLogger = (request, response, next) => {
+    // execute this before moving to the next middleware
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    // move to the next middleware
+    next()
+}
+app.use(requestLogger)
+// the middlewares follows the order they are defined
+// so let's execute all the middlewares before the routes
+// execute the routes
+// and then execute the middleware for unknown routes
+
+
+// routes
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+      })})
+
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+    .then(note => {
+
+      if (note) {
+        response.json(note)
+      } else {
+        // 404 Not Found, since the note doesn't exist
+        response.status(404).end()
+      }
+    })
+    
+    // Pass to the middleware error handler
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note(  {
+    content: body.content,
+    important: body.important,
+  })
+
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  // use a simple JS object, not a Mongoose object
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+// Catch-all middleware for non-existent routes (404)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+  };
+  app.use(unknownEndpoint);
+
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  // throwed if the error is a validation error (for the GET request with an invalid id)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+// 
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
